e39259bf1ce687f1b65bd05090a173422bbfbc6f
Ellen Fawkes <fawkes@ttc.cz>
2017-12-19 13:43:52 +0100

    WebError templates

--- misc/WebError.js
+++ misc/WebError.js
@@ -3,6 +3,22 @@ class WebError extends Error {
     super(message);
     this.status = status;
   }
+
+  static NotFound() {
+    return new WebError("Page Not Found", 404);
+  }
+
+  static Forbidden() {
+    return new WebError("Forbidden", 403);
+  }
+
+  static Unauthorized() {
+    return new WebError("Unauthorized", 401);
+  }
+
+  static ServerError() {
+    return new WebError("Server Error", 500);
+  }
 };
 
 module.exports = WebError;
