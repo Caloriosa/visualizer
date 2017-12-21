345d1a7c28f7ebf0dfaa1fac963fd7ba0e57cdda
Ellen Fawkes <fawkes@ttc.cz>
2017-12-21 15:23:52 +0100

    Two graphs, fake data

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,30 @@ router.get('/ajax/graph', function(req, res, next) {
   res.send(data);
 });
 
+router.get('/ajax/graph2', function(req, res, next) {
+  var data = {
+    labels: [
+      "0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00",
+      "7:00", "8:00", "9:00", "10:00", "11:00", "12:00",
+      "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", 
+      "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"
+    ],
+    datasets: [
+      {
+        label: "Humidity",
+        backgroundColor: "#eeeeee",
+        borderColor: "#000000",
+        fill: false,
+        data: []
+      }
+    ]
+  };
+  for (let i = 0; i < 18; i++) {
+    data.datasets[0].data.push(getRndInteger(0, 100));
+  }
+  res.send(data);
+});
+
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
