fc7c07d333e4d46dac2c90f87eab2b9508343335
Ellen Fawkes <fawkes@ttc.cz>
2017-12-21 15:35:18 +0100

    Excluded line chart drawer to main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,3 +12,24 @@ $(document).ready(function(event) {
     });
   });
 });
+
+function drawLineChart(elementID, dataUrl) {
+  var options = {
+    type: 'line',
+    data: []
+  };
+  var jsonData = $.ajax({
+    url: dataUrl,
+    dataType: 'json'
+  }).done(function (results) {
+    options.data = results
+    // Get the context of the canvas element we want to select
+    var ctx = document.getElementById(elementID + "-canvas");
+
+    // Instantiate a new chart
+    var myLineChart = new Chart(ctx, options);
+  }).fail(function(err) {
+    console.log("error while loading graph " + elementID + " datasource: " + dataUrl);
+    document.getElementById(elementID).innerText = "Error while loading graph!";
+  });
+}
